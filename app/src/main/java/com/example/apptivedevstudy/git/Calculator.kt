4ab32602d441553462969181e9c9466d4f9fd00a
Won Suk <daily142857@gmail.com>
2022-03-26 21:30:35 +0900

    feat: 계산기 기능 구현

--- app/src/main/java/com/example/apptivedevstudy/git/Calculator.kt
+++ app/src/main/java/com/example/apptivedevstudy/git/Calculator.kt
@@ -9,15 +9,15 @@ class Calculator: ICalculator<Int> {
     }
 
     override fun add(a: Int, b: Int): Int {
-
+        return a+b
     }
 
     override fun subtract(a: Int, b: Int): Int {
-
+        return a-b
     }
 
     override fun multiply(a: Int, b: Int): Int {
-
+        return a*b
     }
 
     fun printResult() {
