dbec01e598de0d6091521eeb53b52c5f26f3f9b8
Cotidie <daily142857@gmail.com>
2022-09-26 01:25:30 -0700

    Uploads resources for ui exercise

--- 01 Observer Pattern/app/src/main/java/com/example/apptivedevstudy/debug/dataclass/Location.kt
+++
@@ -1,7 +0,0 @@
-package com.example.apptivedevstudy.debug.dataclass
-
-data class Location(val row: Int, val column: Int) {
-    override fun toString(): String {
-        return "($row, $column)"
-    }
-}
