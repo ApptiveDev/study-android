a8311f78a0666e82554babecd4798e9001dccd8b
이지은 <jieun7056@naver.com>
2024-07-17 22:43:13 +0900

    feat: MainActivity에서 navigation 적용되도록 수정

--- 20th/app/src/main/java/com/apptive/layout/MainActivity.kt
+++ 20th/app/src/main/java/com/apptive/layout/MainActivity.kt
@@ -10,6 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.apptive.layout.jieun.presentation.NavGraph
+import com.apptive.layout.jieun.presentation.Screen
 import com.apptive.layout.jinseo.presentation.LoginScreen
 import com.apptive.layout.junyoung.presentation.SplashScreen
 import com.apptive.layout.ui.theme.LayoutTheme
@@ -19,13 +22,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    LoginScreen()
-                }
+                val navController = rememberNavController()
+                val navGraph = NavGraph()
+                navGraph.SetupNavGraph(
+                    navController = navController,
+                    startScreen = Screen.Splash
+                )
             }
         }
     }
