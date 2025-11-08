1a79f86ca252b91931346139abad9275c22ea07e
mark <mark77234@naver.com>
2025-11-08 17:00:13 +0900

    feat: 피드 추가

--- app/src/main/java/com/apptive/project23/ui/member/Member3Screen.kt
+++ app/src/main/java/com/apptive/project23/ui/member/Member3Screen.kt
@@ -2,6 +2,7 @@
 
 package com.apptive.project23.ui.member
 
+//import androidx.compose.material3.ButtonDefaults.outlinedButtonColors
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,38 +10,59 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AirplanemodeActive
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.Laptop
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.apptive.project23.ui.theme.Project23Theme
 
 @Composable
 
 fun Member3Screen(onBack: () -> Unit) {
+
+    var scrollState = rememberScrollState()
+    var isClicked by remember { mutableStateOf(false) }
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("xhdlf_k") },
+                title = {
+                    Text(
+                        text = "xhdlf_k",
+                        fontWeight = FontWeight.SemiBold
+                    )
+                },
                 navigationIcon = {
                     IconButton(onClick = onBack) {
                         Icon(
@@ -65,15 +87,16 @@ fun Member3Screen(onBack: () -> Unit) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(inner)
-                .padding(horizontal = 20.dp, vertical = 16.dp),
+                .padding(horizontal = 20.dp, vertical = 16.dp)
+                .verticalScroll(scrollState),
             verticalArrangement = Arrangement.spacedBy(20.dp), // 텍스트 사이사이 넓이
 
 
         ) {
 
-            Row (
+            Row(
                 verticalAlignment = Alignment.CenterVertically
-            ){
+            ) {
                 Box( // 프로필 아이콘 하나이므로 옆에 글자를 쓸려면 box를 감싸는 row를 생성
                     modifier = Modifier
                         .background(color = Color(0xFFE0E0E0), shape = CircleShape)
@@ -96,9 +119,9 @@ fun Member3Screen(onBack: () -> Unit) {
 
                         )
                     Spacer(modifier = Modifier.height(8.dp))
-                    Row ( // 각 column 마다 그 윗줄에 spacer을 둬도 돼지만 그것들의 부모인 Row의 속성에 추가
+                    Row( // 각 column 마다 그 윗줄에 spacer을 둬도 돼지만 그것들의 부모인 Row의 속성에 추가
                         horizontalArrangement = Arrangement.spacedBy(24.dp)
-                    ){
+                    ) {
                         Column {
                             Text(
                                 text = "1",
@@ -112,7 +135,7 @@ fun Member3Screen(onBack: () -> Unit) {
                         }
                         Column {
                             Text(
-                                text = "100",
+                                text = "264",
                                 fontWeight = FontWeight.Bold,
                                 modifier = Modifier.padding(start = 4.dp)
 
@@ -124,7 +147,7 @@ fun Member3Screen(onBack: () -> Unit) {
                         }
                         Column {
                             Text(
-                                text = "100",
+                                text = "322",
                                 fontWeight = FontWeight.Bold,
                                 modifier = Modifier.padding(start = 4.dp)
                             )
@@ -133,15 +156,173 @@ fun Member3Screen(onBack: () -> Unit) {
                                 color = Color.Gray
                             )
                         }
+
                     }
+
+                }
+
+            }
+            Column {
+                Row(
+                    // 가로로 가운데에 위치하도록
+                    modifier = Modifier.fillMaxWidth(), // text 크기만큼만 있는데 부모(최상위 Column)를 fillMax로 늘림
+                    horizontalArrangement = Arrangement.Center
+                )
+                {
+
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Text(
+                        text = "https://github.com/minseoriii"
+                    )
+                }
+
+
+                Row(
+                    // 가로로 가운데에 위치하도록
+                    modifier = Modifier.fillMaxWidth(), // text 크기만큼만 있는데 부모(최상위 Column)를 fillMax로 늘림 fillMaxSize하면 가로세로 다 최대로 늘어남
+                    horizontalArrangement = Arrangement.Center
+                )
+                {
+
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Text(
+                        text = "https://blog.naver.com/minseo_324"
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    OutlinedButton( //상태변화
+                        onClick = { isClicked = !isClicked },
+                        modifier = Modifier.weight(1f),
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            contentColor = if (isClicked) Color.White else Color.Black,
+                            containerColor = if (isClicked) Color.Blue else Color.White
+                        )
+                    ) {
+                        Text(
+                            text = if (isClicked == true) "팔로잉" else "팔로우"
+                        )
+
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    OutlinedButton(
+                        onClick = {},
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = "메시지"
+                        )
+
+                    }
+
+
                 }
             }
+
+            Text(
+                text = "하이라이트",
+                fontWeight = FontWeight.Bold
+            )
+
+            Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+
+
+                Highlightitems(title = "스터디", Icon = Icons.Default.Laptop)
+                Highlightitems(title = "프로젝트", Icon = Icons.Default.Book)
+                Highlightitems(title = "여행", Icon = Icons.Default.AirplanemodeActive)
+
+
+            }
+            Text(
+                text = "게시물",
+                fontWeight = FontWeight.Bold
+            )
+            Column (
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ){
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Postingitems()
+                    Postingitems()
+                    Postingitems()
+                }
+
+                Row (
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ){
+                    Postingitems()
+                    Postingitems()
+                    Postingitems()
+                }
+                Row (
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ){
+                    Postingitems()
+                    Postingitems()
+                    Postingitems()
+                }
+
+
+
+            }
+
+
         }
+
+
     }
+
 }
 
-@Preview(showBackground = true)
+
 @Composable
-private fun Member3Preview() {
-    Project23Theme { Member3Screen(onBack = {}) }
+fun Highlightitems(title: String, Icon: ImageVector) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(70.dp)
+                .background(color = Color.LightGray, shape = CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icon,
+                contentDescription = "하이라이트1"
+
+            )
+        }
+
+        Text(
+            text = title
+        )
+
+
+    }
+
+}
+
+@Composable
+fun Postingitems() {
+
+    Box(
+        modifier = Modifier
+            .size(90.dp)
+            .background(Color.LightGray, shape = RectangleShape),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            imageVector = Icons.Default.Star,
+            contentDescription = "게시물"
+
+        )
+    }
+
+
 }
