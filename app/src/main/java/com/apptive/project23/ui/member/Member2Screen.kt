229e9b195f4468f6af919286dfb504a736a91e6d
구민서 <minseo12379@gmail.com>
2025-11-08 16:53:25 +0900

    feat: 인스타 페이지 마지막 작업

--- app/src/main/java/com/apptive/project23/ui/member/Member2Screen.kt
+++ app/src/main/java/com/apptive/project23/ui/member/Member2Screen.kt
@@ -9,31 +9,54 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AccessAlarm
+import androidx.compose.material.icons.filled.Accessibility
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Anchor
+import androidx.compose.material.icons.filled.Audiotrack
+import androidx.compose.material.icons.filled.AutoAwesome
+import androidx.compose.material.icons.filled.BakeryDining
+import androidx.compose.material.icons.filled.Bolt
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Laptop
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.TravelExplore
+import androidx.compose.material3.ButtonDefaults.outlinedButtonColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.apptive.project23.ui.theme.Project23Theme
 
 @Composable
 fun Member2Screen(onBack: () -> Unit) {
+    var isClicked by remember { mutableStateOf(false) }
+    val scrollState = rememberScrollState()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -61,6 +84,7 @@ fun Member2Screen(onBack: () -> Unit) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(inner)
+                .verticalScroll(scrollState)
                 .padding(horizontal = 20.dp, vertical = 16.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp),
         ) {
@@ -126,12 +150,144 @@ fun Member2Screen(onBack: () -> Unit) {
                     }
                 }
             }
+            Column {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(id = com.apptive.project23.R.drawable.github),
+                        contentDescription = "깃허브",
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Text(
+                        text = "https://github.com/nine-minseo"
+                    )
+
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(id = com.apptive.project23.R.drawable.ic_notion),
+                        contentDescription = "노션",
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(5.dp))
+                    Text(
+                        text = "https://www.notion.so/apptive/279e3d4189a580bc8575d1f4b39f0542?source=copy_link"
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    OutlinedButton(
+                        onClick = { isClicked = !isClicked },
+                        modifier = Modifier.weight(1f),
+                        colors = outlinedButtonColors(
+                            contentColor = if (isClicked == true) Color.White else Color.Black,
+                            containerColor = if (isClicked == true) Color.Blue else Color.White
+
+                        )
+                    ) {
+                        Text(
+                            text = if (isClicked) "팔로잉" else "팔로우"
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    OutlinedButton(
+                        onClick = {},
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = "메시지"
+                        )
+                    }
+                }
+            }
+            Text(
+                text = "하이라이트",
+                fontWeight = FontWeight.Bold,
+            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                HightlightItem2(title = "스터디", icon = Icons.Default.Laptop)
+                HightlightItem2(title = "프로젝트", icon = Icons.Default.TravelExplore)
+                HightlightItem2(title = "여행", icon = Icons.Default.Info)
+            }
+
+            Text(
+                text = "게시물",
+                fontWeight = FontWeight.Bold,
+            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Feed(icon = Icons.Default.Add)
+                    Feed(icon = Icons.Default.Person)
+                    Feed(icon = Icons.Default.Anchor)
+                }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Feed(icon = Icons.Default.Accessibility)
+                    Feed(icon = Icons.Default.Audiotrack)
+                    Feed(icon = Icons.Default.AutoAwesome)
+                }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Feed(icon = Icons.Default.Bolt)
+                    Feed(icon = Icons.Default.AccessAlarm)
+                    Feed(icon = Icons.Default.BakeryDining)
+                }
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-private fun Member2Preview() {
-    Project23Theme { Member2Screen(onBack = {}) }
+fun HightlightItem2(title: String, icon: ImageVector) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(70.dp)
+                .background(color = Color.LightGray, shape = CircleShape),
+            contentAlignment = Alignment.Center,
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = "하이라이트1"
+            )
+        }
+        Text(
+            text = title
+        )
+    }
+}
+
+@Composable
+fun Feed(icon: ImageVector) {
+    Box(
+        modifier = Modifier
+            .size(90.dp)
+            .background(color = Color.LightGray),
+        contentAlignment = Alignment.Center,
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = "개시물"
+        )
+    }
 }
